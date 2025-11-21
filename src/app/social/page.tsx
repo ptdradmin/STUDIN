95a6fe290ca1541d85321d58a4ea56a0183c9ed8
Gui Martinien Doba <doba.guimartinien@gmail.com>
2025-11-21 23:31:43 +0000

    develope plus social et que le nom stud'in social soi cliquable

--- src/app/social/page.tsx
+++ src/app/social/page.tsx
@@ -1,17 +1,22 @@
 import PostCard from "@/components/post-card";
 import { getPosts } from "@/lib/mock-data";
 import { Camera } from 'lucide-react';
+import Navbar from "@/components/navbar";
+import Link from "next/link";
 
 export default async function SocialPage() {
     const posts = await getPosts();
     
     return (
         <div className="flex flex-col">
+            <Navbar />
             <header className="sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
                  <div className="container flex h-16 items-center justify-between">
-                    <h1 className="text-2xl font-bold tracking-tight bg-clip-text text-transparent bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500">
-                        STUD'IN Social
-                    </h1>
+                    <Link href="/social">
+                        <h1 className="text-2xl font-bold tracking-tight bg-clip-text text-transparent bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500">
+                            STUD'IN Social
+                        </h1>
+                    </Link>
                     <button>
                         <Camera className="h-6 w-6" />
                     </button>
