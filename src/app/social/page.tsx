c17caa572352465ba9a9150ec3f033871240b25b
Gui Martinien Doba <doba.guimartinien@gmail.com>
2025-11-22 00:23:31 +0000

    utliser ça si c'est possible : C:\Users\Gui Martinien>ssh ptdradmin@wind

--- src/app/social/page.tsx
+++ src/app/social/page.tsx
@@ -1,8 +1,9 @@
+
 import SocialClientPage from "@/components/social-client-page";
 
 export default function SocialPage() {
     return (
-        <div className="flex flex-col min-h-screen">
+        <div className="flex flex-col min-h-screen bg-background">
             <SocialClientPage />
         </div>
     );
