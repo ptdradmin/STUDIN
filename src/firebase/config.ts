d1181c498677865959fbd30ae2fae1b6a823e464
Gui Martinien Doba <doba.guimartinien@gmail.com>
2025-11-22 18:38:14 +0000

    <script type="module">   // Import the functions you need from the SDKs

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyAc8X2IT14tHo80hkg7IzY00jRH4PuB2o4",
   authDomain: "studio-4385357604-24c83.firebaseapp.com",
   projectId: "studio-4385357604-24c83",
-  storageBucket: "studio-4385357604-24c83.appspot.com",
+  storageBucket: "studio-4385357604-24c83.firebasestorage.app",
   messagingSenderId: "1039469624980",
   appId: "1:1039469624980:web:f6c7426a7c7e931406fe81"
 };
