9d6867313c2953b911d6b8b9a8559f16097a0334
Gui Martinien Doba <doba.guimartinien@gmail.com>
2025-11-22 01:19:33 +0000

    const firebaseConfig = {   apiKey: "AIzaSyAc8X2IT14tHo80hkg7IzY00jRH4PuB

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,13 +2,14 @@
 import { FirebaseOptions } from 'firebase/app';
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAc8X2IT14tHo80hkg7IzY00jRH4PuB2o4",
+  authDomain: "studio-4385357604-24c83.firebaseapp.com",
+  projectId: "studio-4385357604-24c83",
+  storageBucket: "studio-4385357604-24c83.appspot.com",
+  messagingSenderId: "1039469624980",
+  appId: "1:1039469624980:web:f6c7426a7c7e931406fe81"
 };
+
 
 export function getFirebaseConfig() {
   if (!firebaseConfig.apiKey) {
