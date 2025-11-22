36096bc477b4406631435c31a32802e0cc225a43
Gui Martinien Doba <doba.guimartinien@gmail.com>
2025-11-22 03:07:09 +0000

    const firebaseConfig = {   apiKey: "AIzaSyAc8X2IT14tHo80hkg7IzY00jRH4PuB

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,10 @@
 export const firebaseConfig = {
-  "projectId": "studio-4385357604-24c83",
-  "appId": "1:1039469624980:web:f6c7426a7c7e931406fe81",
-  "apiKey": "AIzaSyAc8X2IT14tHo80hkg7IzY00jRH4PuB2o4",
-  "authDomain": "studio-4385357604-24c83.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "1039469624980"
+  apiKey: "AIzaSyAc8X2IT14tHo80hkg7IzY00jRH4PuB2o4",
+  authDomain: "studio-4385357604-24c83.firebaseapp.com",
+  projectId: "studio-4385357604-24c83",
+  storageBucket: "studio-4385357604-24c83.appspot.com",
+  messagingSenderId: "1039469624980",
+  appId: "1:1039469624980:web:f6c7426a7c7e931406fe81"
 };
+
+    
