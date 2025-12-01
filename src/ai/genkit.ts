f72eb58be2296e703e38abd059edea84d62a825e
Gui Martinien Doba <doba.guimartinien@gmail.com>
2025-12-01 07:40:35 +0000

    npm i --save @genkit-ai/firebaseInstallez le plug-in @genkit-ai/firebase

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,5 +1,8 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
+import { enableFirebaseTelemetry } from '@genkit-ai/firebase';
+
+enableFirebaseTelemetry();
 
 export const ai = genkit({
   plugins: [googleAI()],
