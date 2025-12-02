1f5320496e2c7d2f57709afa69a480b26e749c3a
Gui Martinien Doba <doba.guimartinien@gmail.com>
2025-12-02 23:47:38 +0000

    ReferenceError: React is not defined     at LeaderboardPage (https://600

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -1,6 +1,7 @@
 
 'use client';
 
+import * as React from 'react';
 import SocialSidebar from '@/components/social-sidebar';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
