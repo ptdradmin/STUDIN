7dc3674f999126dea1c9d5337c4714487f21949b
Gui Martinien Doba <doba.guimartinien@gmail.com>
2025-12-04 14:46:31 +0000

    il ne vas pas sur gmail , regarde

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,6 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyAc8X2IT14tHo80hkg7IzY00jRH4PuB2o4",
-  authDomain: "stud-in.com",
+  authDomain: "studio-4385357604-24c83.firebaseapp.com",
   projectId: "studio-4385357604-24c83",
   storageBucket: "studio-4385357604-24c83.appspot.com",
   messagingSenderId: "1039469624980",
