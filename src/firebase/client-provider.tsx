5fa5538753c5950de61a9194ef0bb487c25c88f3
Gui Martinien Doba <doba.guimartinien@gmail.com>
2025-12-04 19:32:06 +0000

    tu a desactiver la connexion manuel et avec gmail

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -9,10 +9,10 @@ interface FirebaseClientProviderProps {
 }
 
 export function FirebaseClientProvider({ children }: FirebaseClientProviderProps) {
+  // `useMemo` ensures Firebase is only initialized once per client session.
   const firebaseServices = useMemo(() => {
-    // Initialize Firebase on the client side, once per component mount.
     return initializeFirebase();
-  }, []); // Empty dependency array ensures this runs only once on mount
+  }, []); 
 
   return (
     <FirebaseProvider
