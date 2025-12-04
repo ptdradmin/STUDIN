eb109aa3d9ce38ced2fb09994f548f550155dde9
Gui Martinien Doba <doba.guimartinien@gmail.com>
2025-12-04 12:45:55 +0000

    FirebaseError: Firebase: Error (auth/firebase-app-check-token-is-invalid

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -9,6 +9,7 @@ import { getFirestore, Firestore } from 'firebase/firestore';
 import { getStorage, FirebaseStorage } from 'firebase/storage';
 import { firebaseConfig } from '@/firebase/config';
 import FirebaseErrorListener from '@/components/FirebaseErrorListener';
+import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check';
 
 
 interface FirebaseClientProviderProps {
@@ -45,6 +46,19 @@ function initializeFirebase() {
       }
       firebaseApp = initializeApp(firebaseConfig);
     }
+    
+    if (typeof window !== 'undefined') {
+      // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+      // key is the counterpart to the secret key you set in the Firebase console.
+      initializeAppCheck(firebaseApp, {
+        provider: new ReCaptchaV3Provider('6LcimiAsAAAAAEYqnXn6r1SCpvlUYftwp9nK0wOS'),
+
+        // Optional argument. If true, the SDK automatically refreshes App Check
+        // tokens as needed.
+        isTokenAutoRefreshEnabled: true
+      });
+    }
+
 
     return getSdks(firebaseApp);
   }
